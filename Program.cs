d2e499cf28d9ef67c26995667f1767ffa551cc00
BazT <bazetug@gmail.com>
2017-10-10 20:45:51 -0700

    first

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace connectingToDBTESTING
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
+                .UseIISIntegration()
                 .Build();
 
             host.Run();
